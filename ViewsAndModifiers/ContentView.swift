47fd486acfde44af9e9599d8df29fb801e189ac0
estersr <estherramoos@gmail.com>
2024-06-03 21:33:47 -0300

    Environment modifiers

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -10,10 +10,14 @@ import SwiftUI
 struct ContentView: View {
     @State private var useRedText = false
     var body: some View {
-        Button("hello world") {
-            useRedText.toggle()
+        VStack {
+            Text("Gryffindor")
+                .blur(radius: 0)
+            Text("Hufflepuff")
+            Text("Ravenclaw")
+            Text("Slytherin")
         }
-        .foregroundColor(useRedText ? .red : .blue)
+        .blur(radius: 5)
     }
 }
 
